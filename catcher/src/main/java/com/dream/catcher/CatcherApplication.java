8ddf525bb0f43f72068e6ca27ccea21b0fecff72
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-11-18 17:11:44 +0900

    generate project

---
+++ catcher/src/main/java/com/dream/catcher/CatcherApplication.java
@@ -0,0 +1,13 @@
+package com.dream.catcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatcherApplication.class, args);
+	}
+
+}
---
+++ catcher/src/test/java/com/dream/catcher/CatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.catcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
