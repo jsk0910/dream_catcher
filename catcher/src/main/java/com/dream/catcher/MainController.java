ffdb1184ba4f23e9b0b827175848edd8a75bb529
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-11-18 19:32:22 +0900

    commit1

--- catcher/src/main/java/com/dream/catcher/MainController.java
+++ catcher/src/main/java/com/dream/catcher/MainController.java
@@ -2,7 +2,6 @@ package com.dream.catcher;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
